5276b842086293378279fb93b7f45300779a35d1
bjhardcastle <ben.hardcastle@alleninstitute.org>
2026-03-20 23:53:47 +0000

    Update environment and remove old files

--- code/utils.py
+++ code/utils.py
@@ -6,29 +6,10 @@ import codeocean.folder
 import requests
 import aind_session
 
-import models
-
 logger = logging.getLogger(__name__)
 
 S3_DEST_BUCKET = "aind-scratch-data"
 S3_DEST_PREFIX = "dynamic-routing/ibl-gui-output"
-
-
-def validate_locations(locations_input: str) -> dict[str, models.ChannelLocationCCF]:
-    locations_dict = json.loads(locations_input)
-    if not all(k.startswith("channel_") for k in locations_dict.keys()):
-        raise ValueError("Invalid locations input: keys must start with 'channel_'")
-    return {k: models.ChannelLocationCCF(**v) for k, v in locations_dict.items()}
-
-
-def validate_sorted_asset_name(sorted_asset_name_input: str) -> str:
-    # ecephys_816308_2025-08-20_15-24-06_sorted_2025-09-20_07-10-14
-    if not sorted_asset_name_input.startswith("ecephys_"):
-        raise ValueError("Invalid sorted asset name: must start with 'ecephys_'")
-    if "_sorted_" not in sorted_asset_name_input:
-        raise ValueError("Invalid sorted asset name: must contain '_sorted_'")
-    return sorted_asset_name_input
-
 
 def _get_all_subfiles(
     client,
